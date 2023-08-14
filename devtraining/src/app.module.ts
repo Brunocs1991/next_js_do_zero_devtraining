3e1f796b07ad809160dc7c3bb903ca9d672d284f
Bruno <brunocosta.1991@hotmail.com>
2023-08-14 20:12:10 -0300

    Configurando o TypeORM com banco de dados Postgres

--- devtraining/src/app.module.ts
+++ devtraining/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    TypeOrmCoreModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
