0882b10c03eb8f041aa7cb85530b2fda37e7f12b
Bruno <brunocosta.1991@hotmail.com>
2023-08-14 18:50:53 -0300

    Conhecendo os DTOs - Data Transfer Object

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -9,6 +9,8 @@ import {
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { Course } from './entities/course.entity';
+import { CreateCourseDto } from '../../dist/courses/dto/create-course.dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -25,13 +27,13 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body: any) {
-    return this.coursesService.create(body);
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body: any) {
-    return this.coursesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
   }
 
   @Delete(':id')
